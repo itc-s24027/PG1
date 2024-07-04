36ff2593a3f060e92d5bf1cdcaf69c7bac87effb
itc-s24027 <s24027@std.it-college.ac.jp>
2024-07-04 07:59:20 +0000

    p55-p86

---
+++ 2-6.py
@@ -0,0 +1,18 @@
+a = 'abc'
+b = 'xyz'
+print("{} {}".format(a,b))
+
+title = 'name'
+value = 'John Smith'
+print('You {} is {}.'.format(title, value))
+
+age = 20
+print('Your age is {}.'.format(age))
+
+title = 'IQ'
+value = 100
+print('{} = {}'.format(title, value))
+
+# python3.6以降　フォーマット済み文字列リテラル
+a, b = "Hello", "World"
+print(f"{a} {b}")
